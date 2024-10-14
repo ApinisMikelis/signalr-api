ab18b27945a338158e07be499b72e4fcb2ae4a6d
ApinisMikelis <m.apinis92@gmail.com>
2024-10-14 12:03:53 +0300

    Configured CORS

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -4,10 +4,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSignalR();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost",
+        builder => builder.WithOrigins("http://localhost:5173")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials()
+    );
+});
+
 var app = builder.Build();
 
+app.UseRouting();
+app.UseCors("AllowLocalhost");
 app.UseHttpsRedirection();
-
 app.MapHub<MessageHub>("message-hub");
-
 app.Run();
